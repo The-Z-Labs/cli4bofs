923fee2c0e1164ef9a0982977ce2754244e50b80
Michal Ziulek <michalziulek@gmail.com>
2024-02-16 22:32:42 +0100

    'zig build' builds all supported archs, 'zig build -Dtarget=arch-os-abi' builds single arch.

--- build.zig
+++ build.zig
@@ -19,37 +19,62 @@ fn cpuArchStr(arch: std.Target.Cpu.Arch) []const u8 {
 }
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const supported_targets: []const std.Target.Query = &.{
+        .{ .cpu_arch = .x86, .os_tag = .windows, .abi = .gnu },
+        .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .gnu },
+        .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .gnu },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+        .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
+        .{ .cpu_arch = .arm, .os_tag = .linux, .abi = .gnueabihf },
+    };
+
+    const std_target = b.standardTargetOptions(.{ .whitelist = supported_targets });
     const optimize = b.option(
         std.builtin.Mode,
         "optimize",
         "Prioritize performance, safety, or binary size (-O flag)",
     ) orelse .ReleaseSmall;
 
-    const zig_yaml_module = b.dependency("zig_yaml", .{
-        .target = target,
-        .optimize = optimize,
-        .log = false,
-    }).module("yaml");
+    const targets_to_build: []const std.Target.Query = if (b.user_input_options.contains("target"))
+        &.{std_target.query}
+    else
+        supported_targets;
 
-    const bof_launcher_dep = b.dependency("bof_launcher", .{ .optimize = optimize });
-    const bof_launcher_lib = bof_launcher_dep.artifact("bof_launcher_" ++
-        comptime osTagStr(@import("builtin").os.tag) ++ "_" ++
-        cpuArchStr(@import("builtin").cpu.arch));
-    const bof_launcher_api_module = bof_launcher_dep.module("bof_launcher_api");
+    for (targets_to_build) |target_query| {
+        const target = b.resolveTargetQuery(target_query);
 
-    const exe = b.addExecutable(.{
-        .name = "cli4bofs",
-        .root_source_file = .{ .path = thisDir() ++ "/src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+        const zig_yaml_module = b.dependency("zig_yaml", .{
+            .target = target,
+            .optimize = optimize,
+        }).module("yaml");
 
-    exe.linkLibrary(bof_launcher_lib);
-    exe.root_module.addImport("bof-launcher", bof_launcher_api_module);
-    exe.root_module.addImport("yaml", zig_yaml_module);
+        const bof_launcher_dep = b.dependency("bof_launcher", .{ .optimize = optimize });
+        const bof_launcher_lib = bof_launcher_dep.artifact(
+            std.mem.join(
+                b.allocator,
+                "_",
+                &.{ "bof_launcher", osTagStr(target.result.os.tag), cpuArchStr(target.result.cpu.arch) },
+            ) catch unreachable,
+        );
+        const bof_launcher_api_module = bof_launcher_dep.module("bof_launcher_api");
 
-    b.installArtifact(exe);
+        const exe = b.addExecutable(.{
+            .name = std.mem.join(
+                b.allocator,
+                "_",
+                &.{ "cli4bofs", osTagStr(target.result.os.tag), cpuArchStr(target.result.cpu.arch) },
+            ) catch unreachable,
+            .root_source_file = .{ .path = thisDir() ++ "/src/main.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        exe.linkLibrary(bof_launcher_lib);
+        exe.root_module.addImport("bof-launcher", bof_launcher_api_module);
+        exe.root_module.addImport("yaml", zig_yaml_module);
+
+        b.installArtifact(exe);
+    }
 }
 
 inline fn thisDir() []const u8 {
